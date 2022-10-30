86f4f6bba15f40461c82d9715d06f3e88208a0cc
Илья <92045360+000Ilya000@users.noreply.github.com>
2022-10-30 17:35:58 +0400

    +Task

--- Components-kurimshin-ilya/pd-components-f-master/my-app/src/App.js
+++ Components-kurimshin-ilya/pd-components-f-master/my-app/src/App.js
@@ -5,12 +5,15 @@ import './Components/appHeader/bootstrap.min.css';
 import './Components/appHeader/appHeader.css';
 import './Components/appRegistration/appRegistration.css';
 import AppRegistration from './Components/appRegistration/appRegistration';
+import AppNewTask from './Components/appNewtask/app.Newtask';
+import './Components/appNewtask/app.Newtask.css';
 
 function App() {
   return (
     <div className="App">
       <AppHeader></AppHeader>
-      <AppRegistration></AppRegistration>
+      {/* <AppRegistration></AppRegistration> */}
+      <AppNewTask></AppNewTask>
     </div>
   );
 }
